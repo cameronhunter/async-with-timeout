5ed5523d1932c517ec76de59867e8fbdfe3ada84
Cameron Hunter <hello@cameronhunter.co.uk>
2023-07-07 19:56:43 -0700

    Implementation using setTimeout from timers/promises

--- src/withTimeout.ts
+++ src/withTimeout.ts
@@ -1,3 +1,4 @@
+import assert from 'node:assert';
 import { once } from 'node:events';
 import { setTimeout } from 'node:timers/promises';
 
@@ -5,6 +6,16 @@ export async function withTimeout<R>(
     action: (signal: AbortSignal) => Promise<R>,
     options: { timeout: number; signal?: AbortSignal; name?: string }
 ): Promise<R> {
+    assert(
+        Number.isSafeInteger(options.timeout) && Number.isFinite(options.timeout),
+        `Value for "timeout" was invalid. It must be an integer. Received: ${options.timeout}`
+    );
+
+    assert(
+        options.timeout >= 0 && options.timeout < 2 ** 32,
+        `Value for "timeout" was invalid. It must be >= 0 && <= ${2 ** 32 - 1}. Received: ${options.timeout}`
+    );
+
     const name = options?.name || 'The operation';
 
     if (options.signal?.aborted) {
